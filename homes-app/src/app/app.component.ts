96fb4370ac9cc5fd533967d845290837050c0dde
greshchuk-dev <greshchuki@gmail.com>
2026-09-17 20:34:26 +0100

    Added router and routerlink, used ActivatedRoute feature

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -11,11 +12,11 @@ import { HomeComponent } from './home/home.component';
       <img class="brandlogo" src="assets/logo.svg" alt="Logo" aria-hidden="true"/>
     </header>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>`,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent]
+  imports: [HomeComponent, RouterModule]
 })
 export class AppComponent {
   title = 'homes';
