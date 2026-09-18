7403117968655a209748c1ec8b4d4922983a5107
greshchuk-dev <greshchuki@gmail.com>
2026-09-18 20:17:35 +0100

    Added form to details component

--- homes-app/src/app/housing.service.ts
+++ homes-app/src/app/housing.service.ts
@@ -116,4 +116,8 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Application submitted! Name: ${firstName} ${lastName}, Email: ${email}`);
+  }
 }
